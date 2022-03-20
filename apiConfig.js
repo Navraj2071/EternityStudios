4518965c26fc5a57adccc7f32f24db510a9a8aae
Navraj <navraj2071@gmail.com>
2022-03-20 16:29:17 +0530

    Updated api URL

--- apiConfig.js
+++ apiConfig.js
@@ -1,4 +1,4 @@
-// const BASE_URL = 'https://eternityapi.herokuapp.com/'
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "https://eternityapi.herokuapp.com/";
+// const BASE_URL = "http://localhost:8000/";
 
 export default BASE_URL;
