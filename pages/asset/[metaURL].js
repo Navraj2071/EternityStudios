588514a4237ff6165d74c51cf096c4c6ba3ac6bc
Navraj <navraj2071@gmail.com>
2022-03-31 16:25:34 +0530

    Server props correction

--- pages/asset/[metaURL].js
+++ pages/asset/[metaURL].js
@@ -1,9 +1,60 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AssetPage from "../assets/[assetId]";
 import BASE_URL from "../../apiConfig";
 
-const AssetPagewithMeta = ({ assetData }) => {
-  return <>{assetData}</>;
+const AssetPagewithMeta = (queryData) => {
+  const [assetData, setAssetData] = useState({
+    response: "...",
+    assetOwner: "...",
+    assetMetadataURL: "...",
+    assetCollection: "...",
+    assetOnSale: true,
+    assetIsMinted: false,
+    assetPrice: 0,
+    assetContract: "...",
+    assetTokenId: 1,
+    assetNetwork: 1,
+    assetLocalToken: 1,
+    assetImageURL: "...",
+    assetQuery: queryData,
+  });
+  useEffect(() => {
+    poppulateData();
+  }, []);
+
+  const poppulateData = async () => {
+    let metaURL1 = queryData.queryData.replaceAll("questionmarketernity", "?");
+    let metaURL2 = metaURL1.replaceAll("slasheternity", "/");
+    const queryAsset = "https://" + metaURL2.replaceAll("doteternity", ".");
+    let serverData = await getAssetData(queryAsset);
+    if (serverData === "Server error") {
+      let assetData = { response: serverData };
+      setAssetData({ ...assetData });
+    } else {
+      if (serverData["response"] === "NFT doesn't exist") {
+        let assetData = { response: serverData["response"] };
+        setAssetData({ ...assetData });
+      } else {
+        let assetData = {
+          response: serverData["response"],
+          assetOwner: serverData["owner"],
+          assetMetadataURL: serverData["metadataURL"],
+          assetCollection: serverData["collection"],
+          assetOnSale: serverData["on_sale"],
+          assetIsMinted: serverData["is_minted"],
+          assetPrice: serverData["price"],
+          assetContract: serverData["contract_address"],
+          assetTokenId: serverData["token_id"],
+          assetNetwork: serverData["network"],
+          assetLocalToken: serverData["local_token_id"],
+          assetImageURL: serverData["imageURL"],
+          assetQuery: queryAsset,
+        };
+        setAssetData({ ...assetData });
+      }
+    }
+  };
+
   return <>{AssetPage({ assetData })};</>;
 };
 
@@ -27,41 +78,41 @@ const getAssetData = async (assetId) => {
 
 export async function getServerSideProps(context) {
   const queryData = context.params.metaURL;
-  return { props: { assetData: "This is working" + queryData } };
-  let metaURL1 = queryData.replaceAll("questionmarketernity", "?");
-  let metaURL2 = metaURL1.replaceAll("slasheternity", "/");
-  const queryAsset = "https://" + metaURL2.replaceAll("doteternity", ".");
-  const assetData = await getAssetData(queryAsset);
+  return { props: { queryData: queryData } };
+  // let metaURL1 = queryData.replaceAll("questionmarketernity", "?");
+  // let metaURL2 = metaURL1.replaceAll("slasheternity", "/");
+  // const queryAsset = "https://" + metaURL2.replaceAll("doteternity", ".");
+  // const assetData = await getAssetData(queryAsset);
 
-  if (assetData === "Server error") {
-    return { props: { assetData: { response: assetData } } };
-  }
+  // if (assetData === "Server error") {
+  //   return { props: { assetData: { response: assetData } } };
+  // }
 
-  if (assetData["response"] === "NFT doesn't exist") {
-    return {
-      props: {
-        assetData: { response: assetData["response"] },
-      },
-    };
-  }
+  // if (assetData["response"] === "NFT doesn't exist") {
+  //   return {
+  //     props: {
+  //       assetData: { response: assetData["response"] },
+  //     },
+  //   };
+  // }
 
-  return {
-    props: {
-      assetData: {
-        response: assetData["response"],
-        assetOwner: assetData["owner"],
-        assetMetadataURL: assetData["metadataURL"],
-        assetCollection: assetData["collection"],
-        assetOnSale: assetData["on_sale"],
-        assetIsMinted: assetData["is_minted"],
-        assetPrice: assetData["price"],
-        assetContract: assetData["contract_address"],
-        assetTokenId: assetData["token_id"],
-        assetNetwork: assetData["network"],
-        assetLocalToken: assetData["local_token_id"],
-        assetImageURL: assetData["imageURL"],
-        assetQuery: queryAsset,
-      },
-    },
-  };
+  // return {
+  //   props: {
+  //     assetData: {
+  //       response: assetData["response"],
+  //       assetOwner: assetData["owner"],
+  //       assetMetadataURL: assetData["metadataURL"],
+  //       assetCollection: assetData["collection"],
+  //       assetOnSale: assetData["on_sale"],
+  //       assetIsMinted: assetData["is_minted"],
+  //       assetPrice: assetData["price"],
+  //       assetContract: assetData["contract_address"],
+  //       assetTokenId: assetData["token_id"],
+  //       assetNetwork: assetData["network"],
+  //       assetLocalToken: assetData["local_token_id"],
+  //       assetImageURL: assetData["imageURL"],
+  //       assetQuery: queryAsset,
+  //     },
+  //   },
+  // };
 }
