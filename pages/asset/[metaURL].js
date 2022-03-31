42fa099f88de7cf526a39a2aecd1f43c0e046ee0
Navraj <navraj2071@gmail.com>
2022-03-31 12:39:40 +0530

    Revised asset routing.

--- pages/asset/[metaURL].js
+++ pages/asset/[metaURL].js
@@ -1,12 +1,124 @@
-import { useEffect } from "react";
-import AssetPage from "../assets/[assetId]";
+import Navbar from "../../custom_modules/navbar";
+import Footer from "../../custom_modules/footer";
+import React from "react";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import BASE_URL from "../../apiConfig";
 
-const AssetPagewithMeta = ({ assetData }) => {
-  return <>{AssetPage({ assetData })};</>;
+const AssetPage = ({ assetData }) => {
+  const [imageURL, setImageURL] = useState("");
+  const [assetName, setAssetName] = useState("");
+  const [assetDescription, setAssetDescription] = useState("");
+  const [traits, setTraits] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (assetData["response"] === "Success") {
+      setImageURL(assetData["assetImageURL"]);
+      getNftIPFSData();
+    }
+  }, []);
+  const getNftIPFSData = async () => {
+    let ipfsURL = assetData["assetMetadataURL"];
+    let response = await fetch(ipfsURL);
+    let nftData = await response.json();
+    setAssetName(nftData["name"]);
+    setAssetDescription(nftData["description"]);
+    setTraits(nftData["traits"]);
+    return nftData;
+  };
+
+  const getRandomKey = (starterString) => {
+    let randomKey =
+      Math.floor(Math.random() * 1000, 3).toString() + starterString;
+    return randomKey;
+  };
+
+  if (assetData["response"] === "Server error") {
+    return (
+      <>
+        <h2>Server Error...</h2>
+      </>
+    );
+  }
+  if (assetData["response"] === "NFT doesn't exist") {
+    return (
+      <>
+        <h2>Requested asset not found.</h2>
+      </>
+    );
+  }
+
+  const NFTCard = () => {
+    return (
+      <>
+        <div className="nft-mint" style={{ minHeight: "80vh" }}>
+          <img src={imageURL} alt="NFT" />
+          <div className="nft-mint-2">
+            <div className="nft-description">
+              <h1>{assetName}</h1>
+              <h2>{assetDescription}</h2>
+              <h3>Attributes:</h3>
+              {traits.map((trait) => {
+                return (
+                  <React.Fragment key={getRandomKey(trait["trait_ype"])}>
+                    <h3>
+                      {trait["trait_type"]}: {trait["value"]}
+                    </h3>
+                  </React.Fragment>
+                );
+              })}
+              <h2>{!assetData["assetOnSale"] ? "Sold out" : "Available"}</h2>
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <h3>Price: {assetData["assetPrice"]}</h3>
+                <img
+                  src="https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
+                  alt=""
+                  style={{ width: "1em", border: "none", margin: "0" }}
+                />
+              </div>
+              <h3
+                onClick={() => {
+                  router.push("/profile/" + assetData["assetOwner"]);
+                }}
+              >
+                Owner: {assetData["assetOwner"]}{" "}
+                <button
+                  className="btn2"
+                  onClick={() => {
+                    router.push("/profile/" + assetData["assetOwner"]);
+                  }}
+                >
+                  View Profile
+                </button>
+              </h3>
+              <div>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    router.push("/collection/" + assetData["assetCollection"]);
+                  }}
+                >
+                  View Collection
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Navbar />
+      <NFTCard />
+      <Footer />
+    </>
+  );
 };
 
-export default AssetPagewithMeta;
+export default AssetPage;
 
 const getAssetData = async (assetId) => {
   let response = await fetch(
